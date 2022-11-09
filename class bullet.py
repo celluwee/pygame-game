3d6ab1af1031e5b611507197f157f34c59d39175
celluwee <112815501+celluwee@users.noreply.github.com>
2022-11-09 17:36:52 +0300

    класс пулька

---
+++ class bullet.py
@@ -0,0 +1,14 @@
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.transform.scale(bullet_img, (5, 15))
+        self.image.set_colorkey(BLACK)
+        self.rect = self.image.get_rect()
+        self.rect.bottom = y
+        self.rect.centerx = x
+        self.speedy = 10
+    def update(self):
+        self.rect.y -= self.speedy
+        # убить, если он заходит за верхнюю часть экрана
+        if self.rect.bottom < 0:
+            self.kill()
