883d1cc9da30ef93bbb92ffc95e9b9a234676988
BlinkovaAL <114417852+BlinkovaAL@users.noreply.github.com>
2022-11-09 17:37:59 +0300

    Add files via upload
    Добавляю аптечки для восстановления здоровья

---
+++ Класс Аптечка.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+class Aptechka(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.transform.scale(aptechka_img, (45, 30))
+        self.image.set_colorkey(BLACK)
+        self.rect = self.image.get_rect()
+        self.rect.x = random.randrange(WIDTH - self.rect.width)
+        self.rect.y = random.randrange(0, HEIGHT - 590)
+        self.speedy = 2
+        self.speedx = 0
+    def update(self):
+        self.rect.x += self.speedx
+        self.rect.y += self.speedy
+        if self.rect.top > HEIGHT + 10 or self.rect.left < -25 or   self.rect.right > WIDTH + 20:
+            self.rect.y = random.randrange(-100, -40)   
+
