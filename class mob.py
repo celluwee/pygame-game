05a6a44263d4633bb898437b83441118212fcc81
BlinkovaAL <114417852+BlinkovaAL@users.noreply.github.com>
2022-11-16 17:09:39 +0300

    Add files via upload

---
+++ class mob.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+class Mob(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.transform.scale(mob_img, (35, 35))
+        self.image.set_colorkey(BLACK)
+        self.rect = self.image.get_rect()
+        self.rect.x = random.randrange(WIDTH - self.rect.width)
+        self.rect.y = random.randrange(0, HEIGHT - 550)
+        self.speedy = random.randrange(1, 8)
+        self.speedx = random.randrange(-3, 3)
+    def update(self):
+        self.rect.x += self.speedx
+        self.rect.y += self.speedy
+        if self.rect.top > HEIGHT + 10 or self.rect.left < -25 or   self.rect.right > WIDTH + 20:
+            self.rect.x = random.randrange(WIDTH - self.rect.width)
+            self.rect.y = random.randrange(-100, -40)
+            self.speedy = random.randrange(1, 8)
+
+
+# In[ ]:
+
+
+
+
